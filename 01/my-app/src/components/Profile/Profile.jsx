67a2c8a59307bef671f2d7ba7c13bff5a00ae5c3
Azim Azimov <azim-creat@github.com>
2019-10-28 11:39:06 +0600

    84 hooks UseState

--- 01/my-app/src/components/Profile/Profile.jsx
+++ 01/my-app/src/components/Profile/Profile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Profile.module.css';
 import Preloader from '../Preloader/Preloader';
 import {Redirect} from 'react-router-dom'
-import ProfileStatus from "./ProfileStatus"
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks"
 import PostsContainer from "../Posts/PostsContainer"
 
 
@@ -20,7 +20,7 @@ let Profile = (props)=>{
             <div className={styles.profileInfo_img}><img src={props.profile.photos.small} alt=""/></div>
             <div className={styles.profileInfo_text}> 
                 <div className={styles.profileInfo_text_fullName}>{props.profile.fullName}</div>
-                <ProfileStatus profile={props.profile} status={props.status} updateStatus={props.updateStatus} />
+                <ProfileStatusWithHooks profile={props.profile} status={props.status} updateStatus={props.updateStatus} />
                 <div className={styles.profileInfo_text_fullName}>{props.profile.aboutMe}</div>
                 <div className={styles.profileInfo_text_status}>{props.profile.lookingForAJob}</div>
                 <div className={styles.profileInfo_text_rang}>{props.profile.lookingForAJobDescription}</div>
